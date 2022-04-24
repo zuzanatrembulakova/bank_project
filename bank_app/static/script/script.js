ca2207aa4bba8b2a295eddd591637ba25fd62296
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2022-04-24 09:37:57 +0200

    html automatic payment

--- bank_app/static/script/script.js
+++ bank_app/static/script/script.js
@@ -12,3 +12,13 @@ function openModal(id){
 function closeModal(id){
     one('#modal_' + id).style.display = 'none'
 }
+
+function showAutomaticPayment() {
+    let checkBox = one("#aut_payment_check");
+    let details = one("#automatic_payment_details");
+    if (checkBox.checked == true){
+      details.style.display = "block";
+    } else {
+       details.style.display = "none";
+    }
+  }
