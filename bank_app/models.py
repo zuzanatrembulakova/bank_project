f7395d327ffa077c529fe96ec5673e9fc8d3a874
zuzanatrembulakova <zuzana.trembulakova@gmail.com>
2022-04-30 15:28:18 +0200

    sms

--- bank_app/models.py
+++ bank_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
-
 class Ranking(models.Model):
     rType = models.CharField(max_length=200)
     loan = models.BooleanField(default=False)
--- bank_app/views.py
+++ bank_app/views.py
@@ -4,6 +4,7 @@ from .models import *
 from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
 from django.http import HttpResponseRedirect
 from django.db import IntegrityError, transaction
+from twilio.rest import Client
 
 import requests
 
@@ -51,9 +52,16 @@ def login(request):
     context = {}
     if request.method == "POST":
         user = authenticate(request, username=request.POST['name'], password=request.POST['password'])
-        userType = get_user_type(user)        
+        userType = get_user_type(user)       
         if user:
             dj_login(request, user)
+            # client = Client('AC19cbcef853566442dffb9784e745034d','147803e3c78223a9280f4d5b6953c1f1')
+            # message = client.messages.create(
+            #     body='Test message',
+            #     from_='+19707167454',
+            #     to='+4571803342'
+            # )
+            # print(message.body) 
             print(f"Looged in as {userType}")
             return HttpResponseRedirect(reverse('bank_app:index'))
         else:
